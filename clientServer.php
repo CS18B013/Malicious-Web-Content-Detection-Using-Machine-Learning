ee023efe063903a17f9ce2992d3a14e0aa46aac4
CS18B013 <54976288+CS18B013@users.noreply.github.com>
2021-03-18 12:08:42 +0530

    Updated Module 3&4 Commit-1

--- clientServer.php
+++ clientServer.php
@@ -14,17 +14,6 @@ $bytes=file_put_contents('markup.txt', $html);
 //$decision=exec("$python_path test.py $site 2>&1 ");
 
 // Replace the path with the path of your python2.x installation.
-<<<<<<< HEAD
-$decision=exec("C:Python27 test.py $site 2>&1 ");
+$decision=exec("C:\Users\LENOVO\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.9 test.py $site 2>&1 ");
 echo $decision;
 ?>
-
-
-
-//  /Library/Frameworks/Python.framework/Versions/2.7/bin/python2
-=======
-$decision=exec("C:\Users\'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.9 test.py $site 2>&1 ");
-echo $decision;
-?>
-
->>>>>>> 7cd38b040437511e18406ebf2009ab9a49212693
